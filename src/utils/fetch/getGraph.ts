20ca17fdf4c75010a80b5cacea00b654bb9e68e0
Muhun Kim <iam@muhun.kim>
2020-01-12 09:55:35 -0800

    Create NODE_ENV

--- src/utils/fetch/getGraph.ts
+++ src/utils/fetch/getGraph.ts
@@ -1,4 +1,8 @@
 import request, { RequestPromise } from 'request-promise'
 
+const isDevelopment = process.env.NODE_ENV! === 'development'
+
 export default (username: string): RequestPromise<string> =>
-  request.get(`https://github.com/users/${username}/contributions`)
+  isDevelopment
+    ? request.get('http://localhost:5000')
+    : request.get(`https://github.com/users/${username}/contributions`)
