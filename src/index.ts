5911b04fbcf5bf4e03c293303ba61b1f220c94dd
Muhun Kim <iam@muhun.kim>
2020-01-28 18:03:29 +0900

    Implement twitter client

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import makeBanner from './utils/makeBanner'
+import { updateProfileBanner } from './utils/client'
 
 const main = async () => {
   const banner = await makeBanner('x86chi')
+  await updateProfileBanner(banner)
 }
 
 main().catch(e => console.error(e))
