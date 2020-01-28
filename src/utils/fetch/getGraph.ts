ba83b08172759e2d90fb07d51f48cfa68ce4c044
Muhun Kim <iam@muhun.kim>
2020-01-28 19:25:57 +0900

    Remove mock

--- src/utils/fetch/getGraph.ts
+++ src/utils/fetch/getGraph.ts
@@ -3,6 +3,4 @@ import request, { RequestPromise } from 'request-promise'
 const isDevelopment = process.env.NODE_ENV! === 'development'
 
 export default (username: string): RequestPromise<string> =>
-  isDevelopment
-    ? request.get('http://localhost:5000')
-    : request.get(`https://github.com/users/${username}/contributions`)
+  request.get(`https://github.com/users/${username}/contributions`)
