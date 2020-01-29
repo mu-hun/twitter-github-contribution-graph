81f816dff56435c3a2a63e7614031cde1ab5aae1
Muhun Kim <iam@muhun.kim>
2020-01-29 15:37:55 +0900

    Use async readFile in test suite

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,10 @@
 import getGraph from './utils/fetch/getGraph'
 import { getGraphAndParseSvg } from './utils/svg'
-import fs from 'fs'
+import { readFile } from './utils/fs'
 
 test('is same?', async () => {
   const result = await getGraphAndParseSvg('x86chi')
-  const expectSVG = fs.readFileSync('mock/expect.svg', {
-    encoding: 'utf8',
-  })
+  const expectSVG = await readFile('mock/expect.svg', { encoding: 'utf8' })
 
   expect(result).toBe(expectSVG)
 })
