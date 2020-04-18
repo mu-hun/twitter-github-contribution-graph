8953842e17969c1f2ee3d4e0c73b763cdf4e45d5
Muhun Kim <iam@muhun.kim>
2020-04-19 01:47:35 +0900

    Insert XML meta tag in first line

--- src/utils/svg.ts
+++ src/utils/svg.ts
@@ -1,17 +1,21 @@
 import getGraph from './fetch/getGraph'
 import { parseGraphAndCurrentYear, parseGraph } from './parse'
 
+const xmlMetaTag = `<?xml version="1.0" encoding="UTF-8"?>
+`
 const parseSVG = (data: string) => {
   const OnlyHaveCurrentYear = parseGraphAndCurrentYear(data)
   const result = parseGraph(OnlyHaveCurrentYear).html() as string
-  const formatted = result
-    .trim()
-    .replace(/\n   /g, '')
-    .replace(
-      /       <rect/g,
-      `
+  const formatted =
+    xmlMetaTag +
+    result
+      .trim()
+      .replace(/\n   /g, '')
+      .replace(
+        /       <rect/g,
+        `
     <rect`
-    )
+      )
   return formatted
 }
 
