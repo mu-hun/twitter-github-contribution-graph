499a48309671767ccf0b24c970ecc12ec0f1d70a
Muhun Kim <iam@muhun.kim>
2020-04-20 20:08:00 +0900

    Date calcaulate with getTime method
    Solve type error : Date - Date to number - number

--- src/fetch/getWeek.ts
+++ src/fetch/getWeek.ts
@@ -4,6 +4,7 @@ export default function startWeekRange(date: Date) {
   )
   UTC.setUTCDate(UTC.getUTCDate() + 4 - (UTC.getUTCDay() || 7))
   const yearStart = new Date(Date.UTC(UTC.getUTCFullYear(), 0, 1))
-  // @ts-ignore
-  return Math.ceil(((UTC - yearStart) / 86400000 + 1) / 7) + 1
+  return (
+    Math.ceil(((UTC.getTime() - yearStart.getTime()) / 86400000 + 1) / 7) + 1
+  )
 }
