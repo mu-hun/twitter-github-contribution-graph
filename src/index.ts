e42a9e54dac2eb8a8abb350ac203422468b57d05
Muhun Kim <iam@muhun.kim>
2020-04-20 14:56:51 +0900

    Use username variable env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import makeBanner from './utils/makeBanner'
 import { updateProfileBanner } from './utils/client'
 
+import { username } from './env'
+
 const main = async () => {
-  const banner = await makeBanner('x86chi')
+  const banner = await makeBanner(username)
   await updateProfileBanner(banner)
 }
 
