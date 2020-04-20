5750f0d3fbad15656d76d0307b9f5facdd3b6639
Muhun Kim <iam@muhun.kim>
2020-04-20 14:58:52 +0900

    Use named function in getGraph

--- src/utils/fetch/getGraph.ts
+++ src/utils/fetch/getGraph.ts
@@ -3,9 +3,10 @@ import request, { RequestPromise } from 'request-promise'
 
 import { isDevelopment } from '../../env'
 
-export default (username: string) =>
-  isDevelopment
+export default function getGraph(username: string) {
+  return isDevelopment
     ? readFile('mock/index.html', { encoding: 'utf8' })
     : (request.get(
         `https://github.com/users/${username}/contributions`
       ) as RequestPromise<string>)
+}
