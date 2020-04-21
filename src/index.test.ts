97b8d083e1c5bb58063081244f58e6f979128ed3
Muhun Kim <iam@muhun.kim>
2020-04-21 19:16:35 +0900

    Imporve test description in index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,9 +2,10 @@ import getGraph from './fetch/getGraph'
 import parseSVG from './parseSvg'
 import { readFile } from './utils/fs'
 
-test('is same?', async () => {
+test('Compare snapshot', async () => {
   const graph = await getGraph()
   const result = parseSVG(graph, new Date('2020-01-12'))
+
   const expectSVG = await readFile('mock/expect.svg', { encoding: 'utf8' })
 
   expect(result).toBe(expectSVG)
