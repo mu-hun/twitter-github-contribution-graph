fa7708e5780532d3f99f0aa5981b877d5035a151
Muhun Kim <iam@muhun.kim>
2020-04-22 00:27:05 +0900

    Add prettier recommended rules

--- src/makeBanner.ts
+++ src/makeBanner.ts
@@ -1,12 +1,7 @@
 import sharp from 'sharp'
 
-export const svg2Png = (result: Buffer) =>
-  sharp(result)
-    .png()
-    .toBuffer()
+export const svg2Png = (result: Buffer) => sharp(result).png().toBuffer()
 
 export default function makeBanner(input: Buffer) {
-  return sharp('resources/banner.png')
-    .composite([{ input }])
-    .toBuffer()
+  return sharp('resources/banner.png').composite([{ input }]).toBuffer()
 }
