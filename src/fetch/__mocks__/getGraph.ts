c4d571f18f6c75d76f4310718df8d12b49e269f7
Muhun Kim <iam@muhun.kim>
2021-04-10 18:43:13 +0900

    Remove unnecessary async syntax

--- src/fetch/__mocks__/getGraph.ts
+++ src/fetch/__mocks__/getGraph.ts
@@ -1,6 +1,6 @@
 import { readFile } from '../../utils/fs'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default async function getGraph(username: string) {
+export default function getGraph(username: string) {
   return readFile('mock/index.html', { encoding: 'utf8' })
 }
